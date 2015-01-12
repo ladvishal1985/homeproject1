aed4be8f5818753a57ee2f025babdbf9f3a5c319
Vishal Lad <ladvishal1985@gmail.com>
2015-01-12 22:42:41 +0530

    First Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name envirosolutionsApp
+ * @description
+ * # envirosolutionsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('envirosolutionsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+
+function init() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 300,
+            header = document.querySelector("header");
+        if (distanceY > shrinkOn) {
+            classie.add(header,"smaller");
+        } else {
+            if (classie.has(header,"smaller")) {
+                classie.remove(header,"smaller");
+            }
+        }
+    });
+}
+
+window.onload = init();
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,8 @@
+angular.module('envirosolutionsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
